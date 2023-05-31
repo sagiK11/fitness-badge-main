b3056975b8d46a42f208ecf6bba3e83192b5bcf0
Sagi korzack <Sagi1193@gmail.com>
2023-05-31 23:27:35 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,14 @@ USERNAME=sagik11
 REPO=fitness-badge-main
 IDENTIFIER=$USERNAME/$REPO
 TAG=$(echo "${{ github.sha }}" | cut -c1-7)
-# SERVICES=("postgres" "api" "pgadmin" "admin" "app")
-SERVICES=("admin")
+SERVICES=("postgres" "api" "pgadmin" "admin" "app")
 
 docker-compose down -v --remove-orphans
 docker-compose -f $DC_FILE build --parallel
 
-for service in "${SERVICES[@]}"
-do
-    echo "tagging and pushing $service..."
-    docker tag $REPO-$service $IDENTIFIER:$service-$TAG
-    docker push $IDENTIFIER:$service-$TAG 
-done
+# for service in "${SERVICES[@]}"
+# do
+#     echo "tagging and pushing $service..."
+#     docker tag $REPO-$service $IDENTIFIER:$service-$TAG
+#     docker push $IDENTIFIER:$service-$TAG 
+# done
