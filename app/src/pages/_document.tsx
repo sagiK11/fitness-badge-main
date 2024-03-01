e5c2477dc807ff610b07460e48240940c12382c7
Sagi korzack <Sagi1193@gmail.com>
2024-03-01 16:00:14 +0200

    create webmanifest

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -3,7 +3,34 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="heb" data-theme="winter" dir="rtl">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="/manifest.webmanifest"
+          crossOrigin="use-credentials"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#f9fafb"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
